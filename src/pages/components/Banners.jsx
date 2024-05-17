9b71a5883a594e1326f74f69a98d902400ba715a
srbchhabra <47674454+srbchhabra@users.noreply.github.com>
2024-05-17 16:56:40 +0530

    fix undefined

--- src/pages/components/Banners.jsx
+++ src/pages/components/Banners.jsx
@@ -5,21 +5,21 @@ const Banners = ({ images, speed = 5000 }) => {
     <div className="inner h-full">
       <div className="wrapper">
         <section style={{ "--speed": `${speed}ms` }}>
-          {images.map(({ id, image }) => (
+          {images?.map(({ id, image }) => (
          <div className="w-full" key={id}>
               <span className="w-full text-2xl font-semibold text-nowrap" >{image}</span> 
             </div>
           ))}
         </section>
         <section style={{ "--speed": `${speed}ms` }}>
-          {images.map(({ id, image }) => (
+          {images?.map(({ id, image }) => (
            <div className="w-full"  key={id}>
                <span className="w-full text-2xl font-semibold text-nowrap " >{image}</span> 
             </div>
           ))}
         </section>
         <section style={{ "--speed": `${speed}ms` }}>
-          {images.map(({ id, image }) => (
+          {images?.map(({ id, image }) => (
             <div className="w-full" key={id}>
                <span className="w-full text-2xl font-semibold text-nowrap " >{image}</span> 
             </div>
